c2b53b04dc3268c825d06cac18fc61aaef01f668
Ekio <141391736+Eki213@users.noreply.github.com>
2025-04-09 21:53:57 -0300

    Add playGame() function
    - Implements the logic to play a full game of 5 rounds.
    - Keeps track of player and computer scores within its scope.
    - Calls playRound() for each round.
    - Displays the overall winner after all rounds.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//variables to keep track of the players score.
-let humanScore = 0;
-let computerScore = 0;
-
 // returns randomly “rock”, “paper” or “scissors”.
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3) + 1;
@@ -15,27 +11,45 @@ function getHumanChoice() {
     return prompt('Rock, Paper or Scissors?').toLowerCase();
 }
 
-// Plays a single round and logs the result.
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        console.log(`Tie. Both picked ${capitalize(humanChoice)}.`);
-        return;
+// Plays a game of 5 rounds and displays the winner.
+function playGame() {
+    //variables to keep track of the players' score.
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // Plays a single round and logs the result
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            console.log(`Tie. Both picked ${capitalize(humanChoice)}.`);
+            return;
+        }
+
+        let playerWins =
+            (humanChoice == 'scissors' && computerChoice == 'paper') ||
+            (humanChoice == 'paper' && computerChoice == 'rock') ||
+            (humanChoice == 'rock' && computerChoice == 'scissors');
+        
+        if (playerWins) {
+            humanScore++;
+            console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`);
+        } else {
+            computerScore++;
+            console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`);
+        }
     }
 
-    let playerWins =
-        (humanChoice == 'scissors' && computerChoice == 'paper') ||
-        (humanChoice == 'paper' && computerChoice == 'rock') ||
-        (humanChoice == 'rock' && computerChoice == 'scissors');
-    
-    if (playerWins) {
-        humanScore++;
-        console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`);
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore > computerScore) {
+        console.log('You won the game!');
+    } else if (humanScore < computerScore) {
+        console.log('You lost the game!');
     } else {
-        computerScore++;
-        console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`);
+        console.log("It's a tie game!");
     }
 }
-
 // capitalize a word
 function capitalize(word) {
     return word.at(0).toUpperCase() + word.slice(1).toLowerCase();
